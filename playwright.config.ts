aa1d5a5da9fd88c1d8903301429c949b2087b46a
Saraswathi Murugesh <v-samurugesh@microsoft.com>
2025-11-21 17:28:37 +0530

    Updated Configuration File

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     browserName: 'chromium',
-    headless: false,
+    headless: true,
     viewport: null, 
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
